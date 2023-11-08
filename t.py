b366e7e4c4977951dac0a944f98c49577a3d34e3
bobobyk <mykha07@mail.ru>
2023-11-08 09:38:11 +0300

    енго
    еногш

---
+++ t.py
@@ -0,0 +1,45 @@
+#D
+
+# u = input('>>> ')
+# word_1 = u[:u.find(' ')]
+# word_2 = u[u.find(' ') + 1:]
+# print(word_2 + ' ' + word_1)
+
+#E
+
+# u = input('>>> ')
+# a = u.find('f')
+# b = u.rfind('f')
+# if a == -1:
+#     print()
+# elif a == b:
+#     print(a)
+# else:
+#     print(a, b)
+
+#F
+
+# u = input('>>> ')
+# if u.count('f') == 1:
+#     print(-1)
+# elif u.count('f') < 1:
+#     print(-2)
+# else:
+#     print(u.find('f', u.find('f') + 1))
+
+#G
+
+# u = input('>>> ')
+# u = u[:u.find('h')] + u[u.rfind('h') + 1:]
+# print(u)
+
+#X
+
+# u = input('>>> ')
+# a = u[:u.find('h')] 
+# b = u[u.find('h'):u.rfind('h') + 1]
+# c = u[u.rfind('h') + 1:]
+# u = a + b[::-1] + c
+# print(u)
+
+
